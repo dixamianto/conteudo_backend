fd2cae67b2570fee7fc7b82593137251c5ecd711
Iara Ribeiro <iararp05@gmail.com>
2023-04-25 08:41:37 -0300

    Aula 13

---
+++ aula13/form_usuario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de usuário</h1>    
+    <hr>
+
+    <form action="" method="post">
+        <label>Nome</label>
+        <input type="text" name="nome"><br><br>
+
+        <label>Login</label>
+        <input type="text" name="login"><br><br>
+
+        <label>Senha</label>
+        <input type="password" name="senha"><br><br>
+    </form>
+</body>
+</html>
+
+<?php
+
+?>
