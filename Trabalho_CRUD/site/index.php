bd48f5ec2dd17aed5b42c6b1d8772aad66960e33
Iara Ribeiro <iararp05@gmail.com>
2023-06-16 18:35:42 -0300

    trabalho 5

--- Trabalho_CRUD/site/index.php
+++ Trabalho_CRUD/site/index.php
@@ -7,10 +7,9 @@
 
 
 <!-- Receitas -->
+<div class="container">
 <h1>Receitas</h1> 
 <hr>
-<div class="container">
-  
   <div class="row row-cols-1 row-cols-md-3 g-3">
     <?php foreach($receitas as $id => $receita): ?>
           <div class="col">
